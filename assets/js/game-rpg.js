9519a9619d5e5571bc546979c20ed33308849128
Liang Cheng <liangcheng6@gmail.com>
2017-03-06 13:38:52 -0500

    More progress.

--- assets/js/game-rpg.js
+++ assets/js/game-rpg.js
@@ -5,7 +5,8 @@ var allCharacters = {
 	darthMaul:character("Darth Maul", "darthMaul", 150, "http://placehold.it/120x80", null, null, null),
 	luke:character("LukeSkyWalker", "luke", 180, "http://placehold.it/120x80", null, null, null),
 }
-var yourCharacter = null;
+var allCharNames = ['obi', 'darthSid', 'darthMaul', 'luke'];
+var myCharacter = null;
 var availableEnemies = [];
 var defender = null;
 var defeatedEnemies = [];
@@ -23,32 +24,90 @@ $('#availableCharacters .row').append(charHtml);
 charHtml = allCharacters["luke"].createHtmlContent("neutral");
 $('#availableCharacters .row').append(charHtml);
 
+$('#myCharacter').hide();
+$('#availableEnemies').hide();
+$('#fightSection').hide();
+$('#defender').hide();
+$('#restart').hide();
 
+
+
+//State machine: 
+//0: have not chosen a character
+//1: have chosen a character, have not chosen an enemy
+//2: have chosen a enemy,and 
+//3: is fighting
+//4: fight is done.  
+//5: chose another character, reset current play health -> return to 2
+
+var gameState = 0;
+
+var availableEnemiesClick = function() {
+	var data_tag = $(this).attr('data')
+	console.log("yourcharacter: " + data_tag)
+
+	$('#fightSection').show();
+	defender = data_tag;
+	charHtml = allCharacters[defender].createHtmlContent('evil');
+
+	$('#defender .row1').append(charHtml);
+	$('#defender').show();
+
+}
+
+var yourCharacterClick = function() {
+	var data_tag = $(this.attr('data'))
+	console.log("yourcharacter: " + data_tag)
+
+};
 
 $("#availableCharacters .character").click(function() {
-	data_tag = $(this).attr('data');
-	console.log("characters: " + data_tag)
+	if(gameState == 0) {
+		var data_tag = $(this).attr('data');
+		console.log("Chosen character: " + data_tag);
+		//TODO: select your character
+		myCharacter = allCharacters[data_tag];
 
-	//TODO: select your character
+		$('#availableCharacters').hide();
+
+		var myCharHtml = myCharacter.createHtmlContent();
+		$('#myCharacter .row').append(myCharHtml);
+		$('#myCharacter').show();
+
+		availableEnemies = _.without(allCharNames, data_tag);
+		for (var i in availableEnemies) {
+			var charName = availableEnemies[i];
+
+			charHtml = allCharacters[charName].createHtmlContent('neutral');
+			$('#availableEnemies .row').append(charHtml);
+		}
+		$('#availableEnemies .character').click(availableEnemiesClick)
+		$('#availableEnemies').show();
+		gameState = 1;
+	}
+	
 });
 
-$('#yourCharacter .character').click(function() {
-	data_tag = $(this.attr('data'))
-	console.log("yourcharacter: " + data_tag)
 
-})
 
-$('#availableEnemies .character').click(function() {
-	data_tag = $(this.attr('data'))
-	console.log("yourcharacter: " + data_tag)
-})
+// $('#yourCharacter .character').click(function() {
+// 	var data_tag = $(this.attr('data'))
+// 	console.log("yourcharacter: " + data_tag)
 
-$('#fightSection button').click(function() {
+// })
 
-})
+
+
+// $('#availableEnemies .character').click()
+
+
+var fightSectionClikc = function () {
+	var data_tag = $(this).attr('data')
+}
+// $('#fightSection button').click()
 
 $('#defender .character').click(function() {
-	data_tag = $(this.attr('data'))
+	var data_tag = $(this).attr('data')
 	console.log("yourcharacter: " + data_tag)
 })
 
