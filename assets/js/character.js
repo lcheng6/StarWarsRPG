367c877d880b6b1090a41937f8e62d438fab520f
Liang Cheng <liangcheng6@gmail.com>
2017-03-06 12:51:31 -0500

    update to the javascript

--- assets/js/character.js
+++ assets/js/character.js
@@ -10,7 +10,27 @@ var character = function(name, characterDataTag, startHealthUnits, cardImg, musi
 	};
 
 	return {
-		createHtmlContent: function () {
+		createHtmlContent: function (additionalClass) {
+
+			// $('#existingContainer').append(
+			// 	  $('<div/>')
+			// 	    .attr("id", "newDiv1")
+			// 	    .addClass("newDiv purple bloated")
+			// 	    .append("<span/>")
+			// 	      .text("hello world")
+			// 	);
+			var result;
+			var div1 = $('<div/>')
+				.attr("data", priv["characterDataTag"])
+				.addClass("character " + additionalClass);
+			var span1 = $("<span/>").text(priv["name"]);
+			var img1 = $("<img/>").attr('src', priv["cardImg"])
+			var span2 = $("<span/>").text(priv["healthUnit"].toString());
+			div1.append(span1);
+			div1.append(img1);
+			div1.append(span2);
+
+			return div1;
 
 		},
 		getCurrentHealthUnit: function() {
--- assets/js/game-rpg.js
+++ assets/js/game-rpg.js
@@ -1,16 +1,28 @@
-var game = function() {
-	var allCharacters = {
-		obi:character("Obi-Wan Kenobi", "obi", 120, "http://placehold.it/350x150", null, null, null),
-		darthSid:character("Darth Sidious", "darthSid", 120, "http://placehold.it/350x150", null, null, null),
-		darthMaul:character("Darth Maul", "darthMaul", 120, "http://placehold.it/350x150", null, null, null),
-		luke:character("LukeSkyWalker", "luke", 120, "http://placehold.it/350x150", null, null, null),
-	}
-	var yourCharacter = null;
-	var availableEnemies = [];
-	var defender = null;
-	var defeatedEnemies = [];
-
+'use strict'
+var allCharacters = {
+	obi:character("Obi-Wan Kenobi", "obi", 120, "http://placehold.it/120x80", null, null, null),
+	darthSid:character("Darth Sidious", "darthSid", 100, "http://placehold.it/120x80", null, null, null),
+	darthMaul:character("Darth Maul", "darthMaul", 150, "http://placehold.it/120x80", null, null, null),
+	luke:character("LukeSkyWalker", "luke", 180, "http://placehold.it/120x80", null, null, null),
 }
+var yourCharacter = null;
+var availableEnemies = [];
+var defender = null;
+var defeatedEnemies = [];
+
+
+//Initialize the available characters list:
+
+var mychar = allCharacters["obi"];
+var charHtml = allCharacters["obi"].createHtmlContent("neutral");
+$('#availableCharacters .row').append(charHtml);
+charHtml = allCharacters["darthSid"].createHtmlContent("neutral");
+$('#availableCharacters .row').append(charHtml);
+charHtml = allCharacters["darthMaul"].createHtmlContent("neutral");
+$('#availableCharacters .row').append(charHtml);
+charHtml = allCharacters["luke"].createHtmlContent("neutral");
+$('#availableCharacters .row').append(charHtml);
+
 
 
 $("#availableCharacters .character").click(function() {
@@ -39,3 +51,4 @@ $('#defender .character').click(function() {
 	data_tag = $(this.attr('data'))
 	console.log("yourcharacter: " + data_tag)
 })
+
